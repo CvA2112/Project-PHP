85ba9bea0b741c9ed09aa5d474eac03bce9170fe
Cameron van Asperen <cjjk2005@gmail.com>
2023-02-15 13:42:59 +0100

    Week 3

---
+++ site/over-mij.php
@@ -0,0 +1,8 @@
+<php?>
+<nav>
+    <ul>
+        <li>    
+            <a href = "show.php?voornaam=Cameron&achternaam=van%20Asperen&leeftijd=17">Gegevens Cameron</a>
+        </li>
+    </ul>
+</nav>    
--- site/show.php
+++ site/show.php
@@ -1,18 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Show opdracht</title>
-</head>
-<body>
-    <?php
+<?php
 
     echo "<pre>";
-    print_r($_GET);
+    echo $_GET["voornaam"];
+    echo $_GET["achternaam" ];
+    echo $_GET["leeftijd"];
     echo "</pre>"
 
-        ?>
-</body>
-</html>
+    ?>
